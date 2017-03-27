28a54e163fe2015863ef61bd7e363a4e1607ac08
santosh <santosh.ece06@gmail.com>
2017-03-27 12:59:59 +0530

    basics.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by santosh on 3/27/17.
+ */
+var app = angular.module("app", []);
+
+
+app.controller("firstController", function($scope){
+
+    $scope.name = "Hello IRIS";
+
+});
+
+
+
+
+
+
