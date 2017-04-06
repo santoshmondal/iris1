41f55efe6665635ed780e84c0292a4be0f9cd91b
santosh <santosh.ece06@gmail.com>
2017-04-06 12:19:51 +0530

    basics.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -26,21 +26,21 @@ app.controller("tableController", function($scope, NgTableParams){
     var simpleList = [];
 
     simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Iris Pvt Ltd"});
+    simpleList.push({"age":12, "name":"Affixus Pvt Ltd"});
+    simpleList.push({"age":11, "name":"Rediff India Ltd"});
+    simpleList.push({"age":12, "name":"Infosys Pvt Ltd"});
+    simpleList.push({"age":21, "name":"Cts India Ltd"});
+    simpleList.push({"age":22, "name":"Capgemini Pvt Ltd"});
+    simpleList.push({"age":31, "name":"wipro India Ltd"});
+    simpleList.push({"age":32, "name":"Reliance Pvt Ltd"});
+    simpleList.push({"age":41, "name":"TCS India Ltd"});
+    simpleList.push({"age":42, "name":"Tata Pvt Ltd"});
+    simpleList.push({"age":51, "name":"Birla India Ltd"});
+    simpleList.push({"age":52, "name":"Mahindra Pvt Ltd"});
+    simpleList.push({"age":61, "name":"Bajaj India Ltd"});
+    simpleList.push({"age":72, "name":"Hero Pvt Ltd"});
+    simpleList.push({"age":71, "name":"Honda India Ltd"});
+    simpleList.push({"age":82, "name":"Swift Pvt Ltd"});
 
     $scope.defaultConfigTableParams = new NgTableParams({}, { "dataset": simpleList});
 
