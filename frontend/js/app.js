944d9bfda5b8d3461a1ed543d4c16a9a09b29cec
santosh <santosh.ece06@gmail.com>
2017-04-09 10:09:05 +0530

    table

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -123,64 +123,26 @@ app.controller("tableController1", function($scope, $filter,  NgTableParams){
 
 
 
-
-app.controller("tableController2", function($scope, $filter,  NgTableParams){
+/**
+ * http://codepen.io/christianacca/pen/VLqqjP?editors=1010
+ */
+app.controller("tableController2", function($scope, $q, $http, $filter,  NgTableParams){
     // First Param is cofig object, and second param is your data list
     var simpleList = [];
 
-    simpleList.push({"age":11, "name":"Iris India Ltd"});
-    simpleList.push({"age":12, "name":"Affixus Pvt Ltd"});
-    simpleList.push({"age":11, "name":"Rediff India Ltd"});
-    simpleList.push({"age":12, "name":"Infosys Pvt Ltd"});
-    simpleList.push({"age":21, "name":"Cts India Ltd"});
-    simpleList.push({"age":22, "name":"Capgemini Pvt Ltd"});
-    simpleList.push({"age":31, "name":"wipro India Ltd"});
-    simpleList.push({"age":22, "name":"Capgemini Pvt Ltd"});
-    simpleList.push({"age":31, "name":"wipro India Ltd"});
-    simpleList.push({"age":22, "name":"Capgemini Pvt Ltd"});
-    simpleList.push({"age":31, "name":"wipro India Ltd"});
-    simpleList.push({"age":31, "name":"wipro India Ltd"});
-    simpleList.push({"age":31, "name":"wipro India Ltd"});
-    simpleList.push({"age":31, "name":"wipro India Ltd"});
-    simpleList.push({"age":31, "name":"wipro India Ltd"});
-
 
     // CUSTOM-1 CONFIGG
-    var initialParam = {};
+    var initialParam = {"page":1, "count":10};
     $scope.customConfigTableParams2 = new NgTableParams(initialParam, {
         "counts": [5, 10, 20],
-        "getData": function (params) {
-            var filteredData = params.filter() ? $filter('filter')(simpleList, params.filter()) : simpleList;
-            var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : simpleList;
-            params.total(orderedData.length);
-
-
-            var currentPage = params.page();
-            var totalLength = simpleList.length;
-            var perPageCount = params.count();
-
-            var totalPages = (Math.floor(totalLength/perPageCount)) + ((totalLength%perPageCount==0)?0:1);
-            if(currentPage == totalPages) {
-
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-                simpleList.push({"age":31, "name":"wipro India Ltd"});
-
-                params.total(simpleList.length);
-
-                var pageArray = simpleList.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                return pageArray;
-            } else{
-                var pageArray = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                return pageArray;
-            }
+        "getData": function(params) {
+            var deferred = $q.defer();
+            return $http.get('./data.json')
+                .then(function(data) {
+                    params.total(data.data.dataList.length);
+                    //deferred.resolve(data.data.dataList);
+                    return data.data.dataList;
+                });
         }
     });
 });
